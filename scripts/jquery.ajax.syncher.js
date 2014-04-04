cd96c7c931c52cc58a5dddb8eb782a6ed4cfa0dc
Crystal Barton <cbarto11@uncc.edu>
2014-04-04 07:41:15 -0400

    divide checking and synching into groups of five to decrease load on server.

--- scripts/jquery.ajax.syncher.js
+++ scripts/jquery.ajax.syncher.js
@@ -1,16 +1,51 @@
 
 
+
+var connections_to_check = null;
+var connections_to_check_index = 0;
+var connections_to_synch = null;
+var connections_to_synch_index = 0;
+
 /**
  * 
  */
 jQuery(document).ready(
 	function()
 	{
-		jQuery('form.connections-synch-form').CheckConnection();
+		connections_to_check = jQuery('form.connections-synch-form');
+		for( var i = 0; i < 5; i++ )
+		{
+			if( connections_to_check_index < connections_to_check.length )
+			{
+				var connection = connections_to_check[connections_to_check_index];
+				jQuery(connection).CheckConnection();
+			}
+			else
+			{
+				break;
+			}
+			connections_to_check_index++;
+		}			
 		
 		jQuery('button.synch-connections').click( function()
 		{
-			jQuery('form.connections-synch-form').SynchConnection();
+			connections_to_synch = jQuery('form.connections-synch-form');
+			for( var i = 0; i < 5; i++ )
+			{
+				if( connections_to_synch_index < connections_to_synch.length )
+				{
+					var connection = connections_to_synch[connections_to_synch_index];
+					jQuery(connection).SynchConnection();
+				}
+				else
+				{
+					break;
+				}
+				connections_to_synch_index++;
+			}
+			
+			if( connections_to_synch.length > 0 )
+				this.attr( 'disabled', 'disabled' );
 		});
 	}
 );
@@ -72,12 +107,33 @@ jQuery(document).ready(
 					$(form).children('div.check-status')
 						.html('Checking site...done.<br/><span class="notice">'+data['message'])+'</span>';
 				}
+
+				check_next_connection();
 			})
 			.fail(function( jqXHR, textStatus )
 			{
 				$(form).children('div.check-status')
 					.html('Checking site...error.<br/>'+'Request Failed: '+jqXHR.responseText+' - '+textStatus);
+
+				check_next_connection();
 			});			
+		}
+		
+		function check_next_connection()
+		{
+			if( !connections_to_check.length ) return;
+			
+			if( connections_to_check_index < connections_to_check.length )
+			{
+				var connection = connections_to_check[connections_to_check_index];
+				$(connection).CheckConnection();
+				connections_to_check_index++;
+			}
+			else
+			{
+				connections_to_check = null;
+				connections_to_check_index = 0;
+			}
 		}
 		
 		
@@ -142,14 +198,36 @@ jQuery(document).ready(
 				
 				//alert( data['synch-data'] );
 				$(form).parent().parent().children('td.synch').html( data['synch-data'] );
+				
+				check_next_connection();
 			})
 			.fail(function( jqXHR, textStatus )
 			{
 				$(form).children('div.synch-status')
 					.html('Checking site...error.<br/>'+'Request Failed: '+jqXHR.responseText+' - '+textStatus);
+				
+				check_next_connection();
 			});			
 		}
-		
+
+
+		function check_next_connection()
+		{
+			if( !connections_to_synch.length ) return;
+			
+			if( connections_to_synch_index < connections_to_synch.length )
+			{
+				var connection = connections_to_synch[connections_to_synch_index];
+				$(connection).SynchConnection();
+				connections_to_synch_index++;
+			}
+			else
+			{
+				connections_to_synch = null;
+				connections_to_synch_index = 0;
+				jQuery('button.synch-connections').removeAttr( 'disabled' );
+			}
+		}		
 		
 		/**
 		 * Setup the CLAS Sites plugin for each DOMObject.
