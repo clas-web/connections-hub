2ef38de4c9947f6e3db6345283f981947b43717e
Crystal Barton <cbarto11@uncc.edu>
2015-06-02 01:19:16 -0400

    Update WP Plugin synching to include new connections-hub information.

--- classes/model/synch-model.php
+++ classes/model/synch-model.php
@@ -279,7 +279,8 @@ class ConnectionsHub_SynchModel
 	{
 		// determine the Connections Spoke's url.
 		if( substr($url,-1) !== '/' ) $url .= '/';
-		$plugin_page_url = $url . '?connections-spoke-api=get-site';
+		$hub = get_bloginfo( 'name' ).'|'.get_permalink( $id );
+		$plugin_page_url = $url . '?connections-spoke-api=get-connections-data&connection-hub='.urlencode($hub);
 		
 		// contact site for Connections Spoke data.
 		$context = stream_context_create(
