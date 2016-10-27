19b330b15b186a1a7db8544ebe9f89703190b260
Alex Chapin <achapin@gmail.com>
2016-10-27 15:10:23 -0400

    update js to disable changing authors for author users

--- scripts/admin_title_disable.js
+++ scripts/admin_title_disable.js
@@ -3,5 +3,5 @@ jQuery(document).ready(function ($) {
 	$('#title').attr('disabled','disabled');
 	$('#post_name').attr('disabled','disabled');
 	$('#edit-slug-buttons .edit-slug').attr('disabled','disabled');
-	
+	$('#post_author_override').attr('disabled','disabled');
 });
