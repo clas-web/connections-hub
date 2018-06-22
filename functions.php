84bb488ccdfb132ea11aa6321ee9394948a01a47
akforsyt <akforsyt@uncc.edu>
2018-06-22 16:18:14 -0400

    remove forced sorting of connections in admin table

--- functions.php
+++ functions.php
@@ -52,7 +52,6 @@ add_action( 'admin_menu', 'remove_menus', 999 );
 
 // Order the Connections posts by the sort title.
 add_filter( 'posts_join', 'conhub_posts_join', 10, 2 );
-add_filter( 'posts_orderby', 'conhub_posts_orderby', 10, 2 );
 
 // Disable certain connections fields for non-admin users
 wp_register_script('admin_title_disable', CONNECTIONS_HUB_PLUGIN_URL.'/scripts/admin_title_disable.js');
@@ -80,24 +79,6 @@ function conhub_posts_join( $join, $wp_query )
 	global $wpdb;
 	$join .= " LEFT JOIN (SELECT post_id, meta_value AS sort_title FROM $wpdb->postmeta WHERE meta_key = 'sort-title') cttable ON ($wpdb->posts.ID = cttable.post_id) ";
 	return $join;
-}
-endif;
-
-
-/**
- * If a connections post, then sort by the 'sort-title', if it exists.
- * @param  string  $orderby  The current order by statement.
- * @param  WP_Query  $wp_query  The WP_Query object of the query.
- * @return  string  The altered order statement.
- */
-if( !function_exists('conhub_posts_orderby') ):
-function conhub_posts_orderby( $orderby, $wp_query )
-{
-	if( $wp_query->get('post_type') !== 'connection' && !$wp_query->get('connection-group') && !$wp_query->get('connection-link') )
-		return $orderby;
-
-	$orderby = " ISNULL(sort_title) ASC, sort_title ASC, post_date DESC ";
-	return $orderby;
 }
 endif;
 
